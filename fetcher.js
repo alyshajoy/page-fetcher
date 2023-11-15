3cad145224d48b4dc2fc54a5a9600371567edaf4
Alysha Huot <alyshajhuot@gmail.com>
2023-11-15 12:29:06 -0700

    create file that writes contents from URL to new file

---
+++ fetcher.js
@@ -0,0 +1,29 @@
+const request = require("request");
+const fs = require("fs");
+
+const URL = process.argv[2];
+const path = process.argv[3];
+
+let bodyContents = "";
+let length = "";
+
+request(URL, (error, response, body) => {
+
+  if(error) { // only print error if there is an error
+    console.log("error:", error);
+  }
+
+  console.log("response:", response && response.statusCode);
+
+  bodyContents = body; // place data from body into global variable
+  length = bodyContents.length.toString(); // place number of characters in body into global variable
+
+  fs.writeFile(path, bodyContents, err => { // write contents pulled from given URL to the file specified
+    if(err) {
+      console.error("error: ", err);
+    }
+  });
+
+  console.log(`Downloaded and saved ${length} bytes to ${path}`);
+
+});
